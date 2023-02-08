d448def4837ce526c6c24750da39d45e717b8d79
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-02-08 13:41:59 -0300

    Entendendo Streams no Node e criando stream de leitura

---
+++ streams/fundamentals.js
@@ -0,0 +1,22 @@
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++;
+        
+        setTimeout(() => {
+            if (1 > 100) {
+                this.push(null);
+            } else {
+                const buf = Buffer.from(String(i))
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+new OneToHundredStream().pipe(process.stdout);
