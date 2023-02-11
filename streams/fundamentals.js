55b5f4155659693370b06ffa20503bb4df3d351b
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-02-11 10:40:45 -0300

    Aplicando streams no módulo HTTP

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,6 +1,6 @@
 // process.stdin.pipe(process.stdout);
 
-import { Readable } from 'node:stream';
+import { Readable, Transform, Writable } from 'node:stream';
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -19,4 +19,19 @@ class OneToHundredStream extends Readable {
     }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+        
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+new OneToHundredStream().pipe(new InverseNumberStream()).pipe(new MultiplyByTenStream());
