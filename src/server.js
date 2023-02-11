3a950fa6eb6e435aae6a2fbc6fadff97d135426f
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-02-11 12:36:53 -0300

    Corpo da requisição em JSON (Stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -2,18 +2,33 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
+
+    const buffers = []
+
+    for await(const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
 
     if (method === 'GET' && url === '/users') {
         return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+
+        
         users.push({
             id: 1,
-            name: 'John Doe',
-            email: 'johndoe@example.com'
+            name,
+            email,
         })
         return res.writeHead(201).end()
     }
