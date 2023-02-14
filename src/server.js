aea15c61993a84a06db5409fe8ce5ff04e74ecbd
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-02-13 21:56:55 -0300

    Rotas com parâmetros (RegEx)

--- src/server.js
+++ src/server.js
@@ -7,9 +7,13 @@ const server = http.createServer(async (req, res) => {
 
     await json(req, res)
 
-    const route = routes.find(route => route.method === method && route.path === url)
+    const route = routes.find(route => route.method === method && route.path.test(url))
 
     if (route) {
+        const routeParams = req.url.match(route.path)
+
+        console.log(routeParams)
+
         return route.handler(req, res)
     }
 
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,9 @@
 export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-z]+)/g
 
-    console.log(Array.from(path.matchAll(routeParametersRegex)))
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+    const pathRegex = new RegExp(`^${pathWithParams}`)
+    
+    return pathRegex
 }
